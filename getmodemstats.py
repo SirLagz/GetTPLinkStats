66c7aab1ded786e5c2918662c5618083028a79ce
Lawrence Yau <sirlagz@gmail.com>
2015-10-07 11:22:57 +0800

    Updated python script

--- getmodemstats.py
+++ getmodemstats.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
-
+import sys
 import telnetlib
 import argparse
 
 parser = argparse.ArgumentParser(description="Gets TP-Link TD-8840 Modem stats")
 parser.add_argument('--stat',help="Stat to retrieve")
+parser.add_argument('host',help="modem IP address")
+parser.add_argument('user',help="modem username")
+parser.add_argument('pw',help="modem password")
 
 args = parser.parse_args()
 stat = args.stat
-
-host = "192.168.254.254"
-user = "admin"
-pw = "admin"
+host = args.host
+user = args.user
+pw = args.pw
 
 tn = telnetlib.Telnet(host)
 
@@ -20,6 +22,7 @@ tn.write(user+"\n")
 tn.read_until("password:")
 tn.write(pw+"\n")
 tn.write("\n")
+
 tn.read_until("TP-LINK(conf)#")
 tn.write("adsl show info\n")
 output = tn.read_until("cmd:SUCC")
